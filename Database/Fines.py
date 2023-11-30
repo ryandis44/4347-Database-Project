a748f20cebe0451ca26b61489c3d80e12842a4b4
Surya1197 <38446388+Surya1197@users.noreply.github.com>
2023-11-30 14:04:44 -0600

    Update Fines.py
    Attempt #1 at making fully functional Fines class (minus integration with UI).

--- Database/Fines.py
+++ Database/Fines.py
@@ -1,8 +1,57 @@
 import aiomysql
 import asyncio
 import time
-from Database.DatabaseConnector import Database
-from datetime import datetime
-from Database.BookLoans import Borrower
 
-db = Database("Database.BookLoans.py")
+from Database.DatabaseConnector import Database
+from Database.BookLoans import Borrower
+from datetime import datetime
+
+db = Database("fines.py")
+
+
+class Fines:
+    
+    async def calculate_fine(self, loan: list) -> None:
+        due_date = loan[4]
+        date_in = int(time.time())
+        
+        if date_in <= due_date:
+            return  # No fine for on-time returns
+        
+        difference_in_days = (date_in - due_date) // (24 * 3600)
+        fine_amt = round(difference_in_days * 0.25, 2)
+        
+        await self.__update_or_create_fine(loan, fine_amt)
+    
+    async def __update_or_create_fine(self, loan: list, fine_amt: float) -> None:
+        card_id = loan[1]
+        existing_fine = await db.execute(
+            "SELECT fine_amt, paid FROM FINES WHERE "
+            f"loan_id='{loan[0]}'"
+        )
+        
+        if existing_fine:
+            if not existing_fine[1]:  # If not paid
+                if existing_fine[0] != fine_amt:
+                    await db.execute(
+                        "UPDATE FINES SET fine_amt = "
+                        f"'{fine_amt}' WHERE loan_id='{loan[0]}'"
+                    )
+        else:
+            await db.execute(
+                "INSERT INTO FINES (loan_id, fine_amt, paid) VALUES "
+                f"('{loan[0]}', '{fine_amt}', '0')"
+            )
+    
+    async def pay_fine(self, loan_id: str) -> None:
+        loan = await db.execute(
+            "SELECT * FROM BOOK_LOANS WHERE "
+            f"loan_id='{loan_id}'"
+        )
+        if not loan or loan[5]:  # If loan not found or already returned
+            return
+        
+        await db.execute(
+            "UPDATE FINES SET paid = '1' WHERE "
+            f"loan_id='{loan_id}'"
+        )
