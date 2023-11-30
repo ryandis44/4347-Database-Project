597cd7c83ad135493083b3e3384667f7c3b3d8e1
Surya1197 <38446388+Surya1197@users.noreply.github.com>
2023-11-30 12:26:05 -0600

    Update Fines.py

--- Database/Fines.py
+++ Database/Fines.py
@@ -1,6 +1,6 @@
 import time
 from Database.DatabaseConnector import Database
 from datetime import datetime
-from Database.BookLoans import Loaner
+from Database.BookLoans import Borrower
 
 db = Database("Database.BookLoans.py")
