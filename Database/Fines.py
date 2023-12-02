6a7ef6a8284c02ca8e7b623ef5ccb06161903a01
Ryan D™️ <ryandisanti5810@gmail.com>
2023-12-01 22:30:02 -0600

    fines fixed

--- Database/Fines.py
+++ Database/Fines.py
@@ -14,6 +14,18 @@ class Fines:
     def __init__(self, loan: list) -> None:
         self.loan = loan
     
+    async def ainit(self) -> None:
+        await self.calculate_fine()
+    
+    @property
+    def fine_amt(self) -> float:
+        difference_in_days = (int(time.time()) - self.loan[4]) // (24 * 3600)
+        amt = round(difference_in_days * 0.25, 2)
+        return amt if amt > 0 else 0.0
+
+    @property
+    def loan_id(self) -> str: return self.loan[0]
+    
     async def calculate_fine(self) -> None:
         due_date = self.loan[4]
         date_in = int(time.time())
@@ -21,43 +33,29 @@ class Fines:
         if date_in <= due_date:
             return  # No fine for on-time returns
         
-        difference_in_days = (date_in - due_date) // (24 * 3600)
-        fine_amt = round(difference_in_days * 0.25, 2)
-        
-        await self.__update_or_create_fine(self.loan, fine_amt)
+        await self.__update_or_create_fine()
     
-    async def __update_or_create_fine(self, fine_amt: float) -> None:
-        card_id = self.loan[1]
+    async def __update_or_create_fine(self) -> None:
         existing_fine = await db.execute(
             "SELECT fine_amt, paid FROM FINES WHERE "
             f"loan_id='{self.loan[0]}'"
         )
         
-        if existing_fine:
-            if not existing_fine[1]:  # If not paid
-                if existing_fine[0] != fine_amt:
+        if existing_fine != []:
+            if existing_fine[0][1] == "0":  # If not paid
+                if existing_fine[0][0] != self.fine_amt:
                     await db.execute(
                         "UPDATE FINES SET fine_amt = "
-                        f"'{fine_amt}' WHERE loan_id='{self.loan[0]}'"
+                        f"'{self.fine_amt}' WHERE loan_id='{self.loan[0]}'"
                     )
         else:
             await db.execute(
                 "INSERT INTO FINES (loan_id, fine_amt, paid) VALUES "
-                f"('{self.loan[0]}', '{fine_amt}', '0')"
+                f"('{self.loan[0]}', '{self.fine_amt}', '0')"
             )
     
-    async def get_fine_amt(self) -> int:
-        pass
-    
-    async def pay_fine(self, loan_id: str) -> None:
-        loan = await db.execute(
-            "SELECT * FROM BOOK_LOANS WHERE "
-            f"loan_id='{loan_id}'"
-        )
-        if not loan or loan[5]:  # If loan not found or already returned
-            return
-        
+    async def pay_fine(self) -> None:
         await db.execute(
             "UPDATE FINES SET paid = '1' WHERE "
-            f"loan_id='{loan_id}'"
+            f"loan_id='{self.loan_id}'"
         )
--- main.py
+++ main.py
@@ -15,8 +15,10 @@ async def main() -> None:
     # await l.check_out(isbn13=9780452264465)
     
     val = await loan_search("9780452264465")
-    fine = Fines(val)
-    print("")
+    fine = Fines(val[0])
+    await fine.ainit()
+    print(fine.fine_amt)
+    print(fine.loan_id)
     
     
     
